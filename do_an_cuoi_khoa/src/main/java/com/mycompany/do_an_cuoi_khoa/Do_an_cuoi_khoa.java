8263b9bd1af2607bb90cea7f748a460bfb486318
CT080253NguyenGiaThieu <thieucx118@gmail.com>
2024-06-15 17:22:03 +0700

    Do an

--- do_an_cuoi_khoa/src/main/java/com/mycompany/do_an_cuoi_khoa/Do_an_cuoi_khoa.java
+++ do_an_cuoi_khoa/src/main/java/com/mycompany/do_an_cuoi_khoa/Do_an_cuoi_khoa.java
@@ -1,16 +1,25 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.do_an_cuoi_khoa;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
  * @author tuan anh
  */
 public class Do_an_cuoi_khoa {
-
+    public static void show(Calendar c)
+    {
+        System.out.printf("Ngay: %d, Thang: %d, Nam: %d, Gio: %d, Phut: %d, Giay: %d\n", c.get(Calendar.DAY_OF_MONTH), c.get(Calendar.MONTH), c.get(Calendar.YEAR), c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE),c.get(Calendar.SECOND));
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Calendar c = Calendar.getInstance();
+        show(c);
+        c.add(Calendar.MONTH, -2);
+        c.add(Calendar.DAY_OF_MONTH,20);
+        show(c);
     }
 }
