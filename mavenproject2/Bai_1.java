a9fa409e0bb24c730d52393687697c76bb960f0e
Thiều <thieucx118@gmail.com>
2024-06-15 15:48:06 +0700

    Add files via upload

---
+++ mavenproject2/Bai_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bai_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tuan anh
+ */
+public class Bai_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.printf("%lf",a);
+    }
+}
